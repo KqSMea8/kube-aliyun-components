f3cff810af5248c99da17cf875b727c2f0eccb6a
kerry.wang <wrasdf@gmail.com>
2019-02-24 13:23:29 +1100

    update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,14 +6,23 @@ if [ "$#" -lt 2 ]; then
   exit 1
 fi
 
-dcr="docker-compose run --rm"
 cluster=$1
 component=$2
 
-# safe_check() {
-#   support_env=(apollo-my-prod apollo-zft-prod)
-#
-# }
+containsElement () {
+  support_env=(apollo-my-prod apollo-zft-prod)
+  env=$1
+  for e in $support_env; do
+    [[ "$e" = "$env" ]] && return 0;
+  done;
+  echo "Not support -> $env cluster"
+  exit 1;
+}
 
-$(${dcr} ntpl validate -p params/base.yaml -p params/${cluster}.yaml -c $component)
-# $dcr ntpl apply -p params/base.yaml -p params/${cluster}.yaml -c $component
+safe_check() {
+  containsElement $cluster
+}
+
+safe_check
+docker-compose run --rm ntpl validate -p params/base.yaml -p params/${cluster}.yaml -c $component
+docker-compose run --rm ntpl apply -p params/base.yaml -p params/${cluster}.yaml -c $component
